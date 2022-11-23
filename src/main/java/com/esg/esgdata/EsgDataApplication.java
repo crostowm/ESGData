2b0aaf04c035d8dff85983677a472a5b7e1b331e
crostowm <51023737+crostowm@users.noreply.github.com>
2022-11-22 19:56:58 -0500

    Initial commit

---
+++ src/main/java/com/esg/esgdata/EsgDataApplication.java
@@ -0,0 +1,13 @@
+package com.esg.esgdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsgDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsgDataApplication.class, args);
+	}
+
+}
